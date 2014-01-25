75eac1034fada068764e76586a7b8be1b5b5e896
hawelkam <m.hawelka@gmail.com>
2014-01-25 09:42:20 +0100

    initial

---
+++ JavaScript/JavaScriptFromScratch01.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+var myValue = 5;
+var mySecondValue = 7 + 9;
+var myThirdValue = myValue + mySecondValue;
+alert(myThirdValue);
+
+var myName = "Michał";
+var myLastName = "Hawełka";
+var myFullName = myName + " " + myLastName;
+alert(myFullName);
